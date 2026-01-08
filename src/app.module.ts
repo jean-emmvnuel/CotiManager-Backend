c79221b5045299c39e1e230a52681e0672246cc0
jean-emmvnuel <emmanuelahossi667@gmail.com>
2026-01-08 16:16:26 +0000

    update repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PositionsModule } from './positions/positions.module';
-import { EquipesModule } from './equipes/equipes.module';
-import { JoueursModule } from './joueurs/joueurs.module';
 import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [PositionsModule, EquipesModule, JoueursModule, AuthModule],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
